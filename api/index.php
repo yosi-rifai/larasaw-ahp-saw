13e8f29017260bd5cf2f864116133de017e2d333
Yosi Rifa'i Putra <138548209+yosiRifai@users.noreply.github.com>
2024-07-29 08:57:41 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,5 @@
 <?php
 
-// Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
