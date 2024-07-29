8e6d12b0ddd9378d97eafe3a87d0136df8e90413
Yosi Rifa'i Putra <138548209+yosiRifai@users.noreply.github.com>
2024-07-29 09:28:25 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,3 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
-require __DIR__.'/../vendor/autoload.php';
-$app = require_once __DIR__.'/../bootstrap/app.php';
+require __DIR__ . "/../public/index.php";
